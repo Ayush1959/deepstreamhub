5d100344f75107656a39a19f282ef9accb3d503d
Ayush <ayush@trinitymascot.com>
2020-03-03 14:58:40 +0530

    dpsCntrl

---
+++ README.md
@@ -0,0 +1,155 @@
+# deepstream.io-client-php
+
+PHP Client using the dsh HTTP API
+
+# Installing & running tests
+
+Running the tests is a bit trickier as it requires a two-node deepstream cluster, consisting of a Websocket deepstream with a test-provider that answers RPCs and listens for events and a HTTP deepstream that the actual tests are run against:
+
+![Diagram](diagram.png)
+
+- Install PHP - you can get it from e.g. [http://windows.php.net/download/](http://windows.php.net/download/) for windows
+- Add the folder with the executables (e.g. php.exe, php-cli.exe) to your path
+- Download PHP Unit from [https://phpunit.de/](https://phpunit.de/)
+- Move the `phpunit-6.2.1.phar` file to your `deepstream.io-client-php` folder
+- Make it executable via
+
+```bash
+chmod +x phpunit.phar
+```
+
+- Download a local version of Redis and run it on its default port
+- Download the latest deepstream version and unzip it
+- run `git clone git@github.com:deepstreamIO/dsx-connection-http.git` in its lib directory
+- install the plugin via `yarn install`
+- copy the configs in `ds-conf` into your deepstream's conf directory
+- install the redis msg connector using `./deepstream.exe install msg redis`
+- start two deepstream instances with
+
+```bash
+./deepstream.exe start -c conf/config-http.yml
+```
+
+and
+
+```bash
+./deepstream.exe start -c conf/config-ws.yml
+```
+
+- install the test provider in the `deepstream.io-client-php`
+
+```bash
+cd test-provider
+yarn install
+```
+
+- run the test provider
+
+```bash
+node test-provider.js
+```
+
+- run the tests using
+
+```
+php phpunit-6.2.1.phar --bootstrap src\DeepstreamClient.php test\client-test.php
+```
+
+If it all works it looks like this
+![Screenshot](screenshot.png)
+
+## API
+
+### `new DeepstreamClient( $url, $authData )`
+
+Creates the deepstream client
+
+```php
+$client = new DeepstreamClient(
+  'https://api.DeepstreamCentral.com/api/v1',
+  array(
+    'token' => 'xxxx-xxxx-xxxx-xxxx'
+  )
+);
+```
+
+### `setRecord( $recordName, [$path], $data )`
+
+Writes full or partial data to a record
+
+```php
+# Writing full data
+$client->setRecord('user/johndoe', array(
+  'firstname' => 'John',
+  'lastname' => 'Doe',
+  'age' => 32,
+  'pets' => array('hamster', 'cat')
+));
+
+# Writing partial data
+$client->setRecord('user/johndoe', 'age', '33');
+```
+
+### `$client->getRecord( $recordName )`
+
+Reads the data for a given record
+
+```php
+$firstname = $client->getRecord('user/johndoe')->firstname;
+```
+
+### `$client->deleteRecord( $recordName )`
+
+Deletes a record
+
+```php
+$client->deleteRecord('user/johndoe');
+```
+
+### `$client->getRecordVersion( $recordName )`
+
+Retrieves the current version of a record
+
+```php
+$version = $client->getRecordVersion('user/johndoe');
+```
+
+### `$client->makeRpc( $rpcName, [$data] )`
+
+Executes a Remote Procedure Call
+
+```php
+#with data
+$twentyfour = $client->makeRpc('multiply-by-two', 12);
+
+#without data
+$client->makeRpc('logout');
+```
+
+### `$client->emitEvent( $eventName, [$data] )`
+
+Emits an event
+
+```php
+#with data
+$client->emitEvent('new-message', 'hey, what\'s up?');
+
+#without data
+$client->emitEvent('ping');
+```
+
+### `$client->startBatch()`
+
+Starts a set of batch operations that will be executed as a single request
+
+### `$client->executeBatch()`
+
+Executes an existing set of batch operations
+
+```php
+    $client->startBatch()
+    $client->emitEvent( 'new-message', 'hey, what\'s up?' );
+    $client->getRecord( 'user/johndoe' );
+    $client->setRecord( 'user/mike', 'age', 12 );
+    $client->executeBatch();
+```
---
+++ test/client-test.php
@@ -0,0 +1,116 @@
+<?php
+use DeepstreamCentral\DeepstreamClient;
+use PHPUnit\Framework\TestCase;
+
+define('API_URL', 'http://localhost:8000/api/v1');
+define('AUTH_DATA', array('token' => 'fiwueeb-3942jjh3jh23i4h23i4h2'));
+
+final class ClientTest extends TestCase
+{
+//    public function testPresence()
+    //    {
+    //        $client = new DeepstreamClient( API_URL, AUTH_DATA );
+    //        $result = $client->getPresence();
+    //        var_dump($result);
+    //        $this->assertEquals( $result, false );
+    //    }
+
+    public function testRecordDoesNotExist()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $result = $client->getRecord('user/wolfram');
+        $this->assertEquals($result, false);
+    }
+
+    public function testWritesFullRecord()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $result = $client->setRecord('user/wolfram', array('lastname' => 'Hempel'));
+        $this->assertEquals($result, true);
+    }
+
+    public function testReadsRecord()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $result = $client->getRecord('user/wolfram');
+        $this->assertEquals($result->lastname, 'Hempel');
+    }
+
+    public function testGetsRecordVersion()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $result = $client->getRecordVersion('user/wolfram');
+        $this->assertEquals($result, 1);
+    }
+
+    public function testWritesRecordPath()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $setResult = $client->setRecord('user/wolfram', 'age', 32);
+        $this->assertEquals($setResult, true);
+        $getResult = $client->getRecord('user/wolfram');
+        $this->assertEquals($getResult->lastname, 'Hempel');
+        $this->assertEquals($getResult->age, 32);
+    }
+
+    public function testGetsRecordVersionAfterPatch()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $result = $client->getRecordVersion('user/wolfram');
+        $this->assertEquals($result, 2);
+    }
+
+    public function testDeletesRecord()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $resultDelete = $client->deleteRecord('user/wolfram');
+        $this->assertEquals($resultDelete, true);
+        $resultGet = $client->getRecord('user/wolfram');
+        $this->assertEquals($resultGet, false);
+    }
+
+    public function testMakesRpc()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $response = $client->makeRpc('times-two', 7);
+        $this->assertEquals($response, 14);
+    }
+
+    public function testResetsTestProvider()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $response = $client->makeRpc('reset-test-provider');
+        $this->assertEquals($response, 'OK');
+    }
+
+    public function testGetsEmptyEventData()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $response = $client->makeRpc('get-event-info');
+        $this->assertEquals(count($response), 0);
+    }
+
+    public function testEmitsEvent()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $responseEmit = $client->emitEvent('test-event', 'some-data');
+        $this->assertEquals($responseEmit, true);
+        $response = $client->makeRpc('get-event-info');
+        $this->assertEquals(count($response), 1);
+        $this->assertEquals($response[0]->name, 'test-event');
+        $this->assertEquals($response[0]->data, 'some-data');
+    }
+
+    public function testRunsBatchRequest()
+    {
+        $client = new DeepstreamClient(API_URL, AUTH_DATA);
+        $client->startBatch();
+        $this->assertEquals($client->makeRpc('times-two', 11), true);
+        $this->assertEquals($client->makeRpc('times-two', 45), true);
+        $response = $client->executeBatch();
+        $this->assertEquals($response->result, 'SUCCESS');
+        $this->assertEquals(count($response->body), 2);
+        $this->assertEquals($response->body[0]->data, 22);
+        $this->assertEquals($response->body[1]->data, 90);
+    }
+}
